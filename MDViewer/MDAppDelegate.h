123cdfb985402eb23e8b184a21464c6382c25bfd
naoty <naoty.k@gmail.com>
2012-12-03 22:51:08 +0900

    Initial Commit

---
+++ MDViewer/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  MDViewer
+//
+//  Created by naoty on 2012/12/03.
+//  Copyright (c) 2012年 naoty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MDViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDViewer
+//
+//  Created by naoty on 2012/12/03.
+//  Copyright (c) 2012年 naoty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDAppDelegate class]));
+    }
+}
