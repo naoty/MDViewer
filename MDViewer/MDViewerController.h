ac206545e9cf76900be24eea353722813b28434b
naoty <naoty.k@gmail.com>
2012-12-05 00:32:07 +0900

    Add MDViewerController

---
+++ MDViewer/MDViewerController.h
@@ -0,0 +1,13 @@
+//
+//  MDViewerController.h
+//  MDViewer
+//
+//  Created by naoty on 2012/12/05.
+//  Copyright (c) 2012年 naoty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDViewerController : UIViewController
+
+@end
---
+++ MDViewer/MDViewerController.m
@@ -0,0 +1,38 @@
+//
+//  MDViewerController.m
+//  MDViewer
+//
+//  Created by naoty on 2012/12/05.
+//  Copyright (c) 2012年 naoty. All rights reserved.
+//
+
+#import "MDViewerController.h"
+
+@interface MDViewerController ()
+
+@end
+
+@implementation MDViewerController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
